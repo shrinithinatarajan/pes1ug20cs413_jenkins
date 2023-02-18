7206a2bb1392f03007158f187a94f30a4c9ae769
shrinithi natarajan <kavinattu2001@gmail.com>
2023-02-18 17:26:46 +0530

    modified hello.cpp for task 4

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run. My srn is PES1UG20CS413\n";
+  cout << "I have successfully built and run. My srn is PES1UG20CS413. Task 4 change implemented\n";
   return 0;
 }
