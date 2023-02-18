2a619b87467b983d8bb8aaad231a2058b7a1d637
shrinithi natarajan <kavinattu2001@gmail.com>
2023-02-18 15:05:38 +0530

    modified hello.cpp

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run. My srn is PES1UG20CS413\n";
+  return 0;
+}
